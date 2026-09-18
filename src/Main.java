e7afa45ff4ff951dc7f548db555f3f113574318d
Oleksandr Yarovyi <olexandryarovyi@gmail.com>
2026-09-18 23:37:42 +0100

    Updated the README, Made Main class to open and parse my file, created enums and class for future tokens, started creating lexer funciton for now its just a placeholder

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Optional;
+
+enum TokenType{
+    _return,
+    int_lit,
+    semicolon
+}
+
+class Token{
+    public TokenType type;
+    public Optional<String> value;
+}
+
 public class Main {
-    public static void main(String[] args){
-        if(args.length != 0)
-            System.out.println(args[0]);
+    //lexer function
+    public static ArrayList<Token> toTokens(String str){
+        for(char c : str.toCharArray())
+            System.out.println(c);
+        return null;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Incorrect usage! - Correct usage samle - oyl <sample.oyl>");
+            System.exit(1);
+        }
+        String contents = Files.readString(Path.of(args[0]));
+        toTokens(contents);
+        System.exit(0);
     }
 }
